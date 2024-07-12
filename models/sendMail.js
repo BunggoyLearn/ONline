b9a016682cf97324de757bc80c91410386a7bce9
CrisSal2 <salgado.chris.m@gmail.com>
2024-07-11 19:22:30 -0700

    created an email format for our emailer

--- models/sendMail.js
+++ models/sendMail.js
@@ -2,6 +2,7 @@
 
 const nodemailer = require("nodemailer");
 require("dotenv").config();
+const path = require("path");
 
 // creates reusable transporter object
 const transporter = nodemailer.createTransport({
@@ -13,4 +14,34 @@ const transporter = nodemailer.createTransport({
       user: process.env.USER,
       pass: process.env.APP_PASSWORD,
     },
-  });
+});
+
+// Email format
+const mailOptions = {
+
+    from: {
+        name: 'OnLine App',
+        address: process.env.USER, // OR => 'OnLineApp@gmail.com'
+    },
+
+    to: [
+        "cris@example.com",
+        "jordan@example.com",
+        "ryan@example.com",
+        "bung@example.com"
+    ],
+
+    cc: [], // It's here if we want it
+
+    subject: "Subject of the email",
+    text: "Hello world?",
+    html: "<b>Hello world?</b>", // html body
+
+    // Adds attaachments to our emailer
+    attachments: [{
+        filename: 'example.jpg',
+        path: path.join(__dirname, 'example.jpg'),
+        contentType: 'image/jpg'
+    },
+    ]
+};
