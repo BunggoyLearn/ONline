42c7396faeeed849582d0ac7d15fefaa4f0b4460
Ryan Petersen <rp26898@gmail.com>
2024-07-11 19:38:58 -0700

    Updated our sequelize.js a bit.

--- config/sequalize.js
+++ config/sequalize.js
@@ -5,13 +5,24 @@ const Sequelize = require('sequelize');
 // This loads our environment variables from .env file
 dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
 
-// This loads and parses config.json
-const config = require('./config.json');
+// Loads our database configuration from config.json based on the current NODE_ENV. This will defaulting to the development enviroment.
+const env = process.env.NODE_ENV || 'development';
+const config = require(path.join(__dirname, 'config.json'))[env];
 
 // This initializes Sequelize with the appropriate environment config
-const sequelize = new Sequelize(config[process.env.NODE_ENV || 'development']);
+const sequelize = new Sequelize(
+  config.database, 
+  config.username, 
+  config.password, 
+  {
+    host: config.host,
+    port: config.port,
+    dialect: config.dialect,
+    logging: false
+  }
+);
 
-// This will test DB connection on application initialization
+// This will test our DB connection on application initialization
 sequelize.authenticate()
   .then(() => {
     console.log('Database connection has been established successfully.');
