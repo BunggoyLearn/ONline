6ef1305bc006770768e887a9732b72bb9e3b8238
CrisSal2 <salgado.chris.m@gmail.com>
2024-07-11 19:50:02 -0700

    added function to the emailer

--- models/sendMail.js
+++ models/sendMail.js
@@ -1,5 +1,5 @@
 // Package Imports
-
+const { log } = require("console");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 const path = require("path");
@@ -8,11 +8,11 @@ const path = require("path");
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     host: "smtp.gmail.com",
-    port: 3001,
+    port: 3001, // Port 587 is used for emails apparently. let's see how it works with 3001
     secure: false, // Use `true` for port 465, `false` for all other ports
     auth: {
-      user: process.env.USER,
-      pass: process.env.APP_PASSWORD,
+        user: process.env.USER,
+        pass: process.env.APP_PASSWORD,
     },
 });
 
@@ -20,8 +20,8 @@ const transporter = nodemailer.createTransport({
 const mailOptions = {
 
     from: {
-        name: 'OnLine App',
-        address: process.env.USER, // OR => 'OnLineApp@gmail.com'
+        name: 'ONline App',
+        address: process.env.USER, // OR => 'ONlineApp@gmail.com'
     },
 
     to: [
@@ -31,17 +31,30 @@ const mailOptions = {
         "bung@example.com"
     ],
 
-    cc: [], // It's here if we want it
+    cc: [], // It's here if we want to use it. Otherwise we coud delete it..
 
     subject: "Subject of the email",
-    text: "Hello world?",
-    html: "<b>Hello world?</b>", // html body
+    text: "This is a test email",
+    html: "<b>This is a test email</b>", // html body
 
-    // Adds attaachments to our emailer
-    attachments: [{
+    // Adds attachments to our emailer / We can add more in the array
+    attachments: [
+        {
         filename: 'example.jpg',
         path: path.join(__dirname, 'example.jpg'),
         contentType: 'image/jpg'
-    },
+        },
     ]
 };
+
+// Sends email
+const sendMail = async (transporter, mailOptions) => {
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log('Email has been sent!');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+sendMail(transporter, mailOptions);
