653d730ed21405fa9554e77c485723f0d730fbc9
CrisSal2 <salgado.chris.m@gmail.com>
2024-07-17 18:43:42 -0700

    setup routes

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+// Package Imports
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+router.get('', (req, res) => {
+    res.sendFile(path.join(__dirname, '', ''))
+});
+
+
+
+
+module.exports = router;
