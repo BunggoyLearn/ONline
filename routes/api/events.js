232caff26f55426bed2d712a0a9473259039ad90
Ryan Petersen <rp26898@gmail.com>
2024-07-21 20:01:51 -0700

    debugging the debugging

--- routes/api/events.js
+++ routes/api/events.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Event } = require('../models'); // Ensure the model path is correct
+const { Event } = require('../../models');
 
 router.get('/events', async (req, res) => {
     try {
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require("express-handlebars");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const routes = require("./routes");
-const eventRoutes = require('./routes/events');
+const eventRoutes = require('./routes/api/events');
 const sequelize = require("./config/connection");
 const helpers = require("./utils/helper");
 
@@ -47,19 +47,6 @@ app.get('/events', (req, res) => {
     console.error(err);
     res.status(500).json(err);
   });
-});
-
-app.use(eventRoutes)
-
-router.get('/events', async (req, res) => {
-  try {
-      const eventsData = await Event.findAll();
-      const events = eventsData.map(event => event.get({ plain: true }));
-      res.render('events', { events, loggedIn: req.session.loggedIn });
-  } catch (error) {
-      console.error('Error fetching events:', error);
-      res.status(500).send('Error fetching events');
-  }
 });
 
 sequelize.sync({ force: false }).then(() => {
