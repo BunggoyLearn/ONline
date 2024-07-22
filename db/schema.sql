26b7bdaac42e0af377ce5212e4fa8438e0c91e15
Ryan Petersen <rp26898@gmail.com>
2024-07-21 20:52:03 -0700

    debuggging recent merge content

--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,7 @@ CREATE DATABASE online_db;
 
 -- Create the users table
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
+  user_id SERIAL PRIMARY KEY,
   username VARCHAR(255) NOT NULL UNIQUE,
   email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
