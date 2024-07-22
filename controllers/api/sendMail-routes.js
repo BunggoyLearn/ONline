d72d859115d81e0b96882e328bdc38994b86ea5c
CrisSal2 <salgado.chris.m@gmail.com>
2024-07-22 15:59:11 -0700

    added path package and routes for mailer

--- controllers/api/sendMail-routes.js
+++ controllers/api/sendMail-routes.js
@@ -6,3 +6,26 @@ router.post('/', (req, res) => {
 });
 
 module.exports = router;
+
+
+/*****************************************     *****************************************/
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const app = express();
+const server = http.Server(app);
+const port = 465;
+
+app.set("port", port);
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public/events.js")));
+
+// Routes
+
+app.get("/", function(req, res){
+  response.sendFile(path.join(__dirname, "views/events.handlebars"))
+});
+
