9309e069ac13aeca344049271b7f24cd4f5abe25
Jordan <jordan.gonzales17@hotmail.com>
2024-07-22 16:35:00 -0700

    Adding Info to the about us page with the styling

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,18 @@
-const router = require('express').Router();
-const dashboardRoutes = require('./dashboard');
-const homeRoutes = require('./home-controller');
-const apiRoutes = require('./api');
+const router = require("express").Router();
+const dashboardRoutes = require("./dashboard");
+const homeRoutes = require("./home-controller");
+const apiRoutes = require("./api");
 
-router.use('/dashboard', dashboardRoutes);
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+router.use("/dashboard", dashboardRoutes);
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
 
-router.get('/events', async (req, res) => {
+router.get("/events", async (req, res) => {
   try {
     const eventData = await Event.findAll();
-    const events = eventData.map(event => event.get({ plain: true }));
-    
-    res.render('events', { events, loggedIn: req.session.loggedIn });
+    const events = eventData.map((event) => event.get({ plain: true }));
+
+    res.render("events", { events, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.error(err);
     res.status(500).json(err);
