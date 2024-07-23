faa573b8b124dc474c4c457e74b94dc2afcd29d0
JordanGWiz <164554206+JordanGWiz@users.noreply.github.com>
2024-07-22 18:34:43 -0700

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,20 @@
-const router = require("express").Router();
-const dashboardRoutes = require("./dashboard");
-const homeRoutes = require("./home-controller");
-const apiRoutes = require("./api");
+const router = require('express').Router();
+const dashboardRoutes = require('./dashboard');
+const homeRoutes = require('./home-controller');
+const apiRoutes = require('./api');
+const eventsRoutes = require('./events')
 
-router.use("/dashboard", dashboardRoutes);
-router.use("/", homeRoutes);
-router.use("/api", apiRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+router.use('/events', eventsRoutes);
 
-router.get("/events", async (req, res) => {
+router.get('/events', async (req, res) => {
   try {
     const eventData = await Event.findAll();
-    const events = eventData.map((event) => event.get({ plain: true }));
-
-    res.render("events", { events, loggedIn: req.session.loggedIn });
+    const events = eventData.map(event => event.get({ plain: true }));
+    
+    res.render('events', { events, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.error(err);
     res.status(500).json(err);
