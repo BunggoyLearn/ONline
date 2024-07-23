053b09327c2a5b98689245f1cca27fb59b054a6e
Bung <android.sitko@gmail.com>
2024-07-22 17:53:23 -0700

    modified routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,4 @@
 const express = require('express');
 const router = express.Router();
-const eventsRoutes = require('./Event-Routes');
-
-router.use('/events', eventsRoutes);
 
 module.exports = router;
